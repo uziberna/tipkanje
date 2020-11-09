d812f8d540f2093d32b4bb0d9cc989e9d67a8512
uziberna <uros.ziberna@gmail.com>
2020-11-10 00:54:09 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,253 @@
+var trenutnaVaja = 1;
+var trenutnaVrstica = 0;
+var napakeVaja = 0;
+var napakeProc = 0;
+var udarciVaja = 0;
+var udarciMin = 0;
+var ure = 0;
+var minute = 0;
+var sekunde = 0;
+var opravljeneVaje = 0;
+var vseUre = 0;
+var vseMinute = 0;
+var vseSekunde = 0;
+var skupniUdarciMin = 0;
+var vsiUdarci = 0;
+var vseNapake = 0;
+var vseNapakeProc = 0;
+
+var zacetekVaje = 0;
+var zacetekVaj = 0;
+
+var izbraneVaje = 0;
+var konecVaje = 0;
+
+var casovnaFunkcija = setInterval(osveziCas, 250);
+
+function osveziCas(){
+    if (zacetekVaj != 0){
+        var tmp = new Date().getTime();
+
+        if (konecVaje == 0){
+            if (zacetekVaje != 0){
+                var casVaje = tmp - zacetekVaje;
+                ure = Math.floor(casVaje / 3600000);
+                casVaje = casVaje - (ure * 3600000);
+                minute = Math.floor(casVaje / 60000);
+                casVaje = casVaje - (minute * 60000);
+                sekunde = Math.floor(casVaje / 1000);
+            }
+
+            var casVaj = tmp - zacetekVaj;
+            vseUre = Math.floor(casVaj / 3600000);
+            casVaj = casVaj - (vseUre * 3600000);
+            vseMinute = Math.floor(casVaj / 60000);
+            casVaj = casVaj - (vseMinute * 60000);
+            vseSekunde = Math.floor(casVaj / 1000);
+        }
+    }
+
+    posodobi();
+}
+
+function izracunUdarcevMin(u, m, s, udarci){
+    var tmp = (u * 3600) + (m * 60) + s;
+    return Math.round(((udarci / tmp) * 60));
+}
+
+function reset(){
+    trenutnaVaja = 1;
+    trenutnaVrstica = 0;
+    napakeVaja = 0;
+    napakeProc = 0;
+    udarciVaja = 0;
+    udarciMin = 0;
+    ure = 0;
+    minute = 0;
+    sekunde = 0;
+    konecVaje = 0;
+    zacetekVaje = 0;
+
+    opravljeneVaje = 0;
+    vseUre = 0;
+    vseMinute = 0;
+    vseSekunde = 0;
+    skupniUdarciMin = 0;
+    vsiUdarci = 0;
+    vseNapake = 0;
+    vseNapakeProc = 0;
+    zacetekVaj = 0;
+
+    document.getElementById("trenutnaVaja").innerHTML = trenutnaVaja;
+    document.getElementById("naloga").innerHTML = trenutnaVaja;
+    document.getElementById("napakeVaja").innerHTML = napakeVaja;
+    document.getElementById("napakeProc").innerHTML = napakeProc;
+    document.getElementById("udarciVaja").innerHTML = udarciVaja;
+    document.getElementById("udarciMin").innerHTML = udarciMin;
+    document.getElementById("ure").innerHTML = ure;
+    document.getElementById("minute").innerHTML = minute;
+    document.getElementById("sekunde").innerHTML = sekunde;
+
+    document.getElementById("opravljeneVaje").innerHTML = opravljeneVaje;
+    document.getElementById("vseUre").innerHTML = vseUre;
+    document.getElementById("vseMinute").innerHTML = vseMinute;
+    document.getElementById("vseSekunde").innerHTML = vseSekunde;
+    document.getElementById("skupniUdarciMin").innerHTML = skupniUdarciMin;
+    document.getElementById("vsiUdarci").innerHTML = vsiUdarci;
+    document.getElementById("vseNapake").innerHTML = vseNapake;
+    document.getElementById("vseNapakeProc").innerHTML = vseNapakeProc;
+}
+
+function posodobi(){
+    document.getElementById("trenutnaVaja").innerHTML = trenutnaVaja;
+    document.getElementById("naloga").innerHTML = trenutnaVaja;
+    document.getElementById("napakeVaja").innerHTML = napakeVaja;
+    document.getElementById("napakeProc").innerHTML = napakeProc;
+    document.getElementById("udarciVaja").innerHTML = udarciVaja;
+    document.getElementById("udarciMin").innerHTML = udarciMin;
+    document.getElementById("ure").innerHTML = ure;
+    document.getElementById("minute").innerHTML = minute;
+    document.getElementById("sekunde").innerHTML = sekunde;
+
+    document.getElementById("opravljeneVaje").innerHTML = opravljeneVaje;
+    document.getElementById("vseUre").innerHTML = vseUre;
+    document.getElementById("vseMinute").innerHTML = vseMinute;
+    document.getElementById("vseSekunde").innerHTML = vseSekunde;
+    document.getElementById("skupniUdarciMin").innerHTML = skupniUdarciMin;
+    document.getElementById("vsiUdarci").innerHTML = vsiUdarci;
+    document.getElementById("vseNapake").innerHTML = vseNapake;
+    document.getElementById("vseNapakeProc").innerHTML = vseNapakeProc;
+}
+
+function naloziVaje(vaje){
+    var nalozi;
+    if (vaje == 0){
+        nalozi = asdf;
+    }
+    else if (vaje == 1){
+        nalozi = asdfa;
+    }
+    else if (vaje == 2){
+        nalozi = fdsa;
+    }
+    else if (vaje == 3){
+        nalozi = fdsa2;
+    }
+
+    return nalozi;
+}
+
+function preveriVaje(vaje){
+    return vaje.length;
+}
+
+function naloziVajo(vaje, vaja){
+    var tmp = naloziVaje(vaje);
+    var nalozi = tmp[vaja];
+
+    document.getElementById("prva").innerHTML = nalozi[0];
+    document.getElementById("druga").innerHTML = nalozi[1];
+    document.getElementById("tretja").innerHTML = nalozi[2];
+
+    return nalozi;
+}
+
+function konecNabora(){
+    document.getElementById("prva").innerHTML = "Čestitamo!";
+    document.getElementById("druga").innerHTML = "Zaključili ste vse vaje iz izbranega nabora!";
+    document.getElementById("tretja").innerHTML = "";
+}
+
+function oznaciVrstico(vrstica){
+    document.getElementById("prva").classList.remove("text-success");
+    document.getElementById("druga").classList.remove("text-success");
+    document.getElementById("tretja").classList.remove("text-success");
+
+    if (vrstica == 0){
+        document.getElementById("prva").classList.add("text-success");
+    }
+    else if (vrstica == 1){
+        document.getElementById("druga").classList.add("text-success");
+    }
+    else {
+        document.getElementById("tretja").classList.add("text-success");
+    }
+}
+
+function naloziVrstico(vaje, vaja, vrstica){
+    var tmp = naloziVajo(vaje, vaja);
+    if (vrstica == 0){
+        return tmp[0];
+    }
+    else if (vrstica == 1){
+        return tmp[1];
+    }
+    else{
+        return tmp[2];
+    }
+}
+
+function preveriVrstico(a, b){
+    if (a == b){
+        return true;
+    }
+}
+
+function preveriVnos(){
+    if (zacetekVaj == 0){
+        zacetekVaj = new Date().getTime();
+    }
+    if (zacetekVaje == 0){
+        zacetekVaje = new Date().getTime();
+    }
+    if (konecVaje == 0){
+        var vnos = document.getElementById("vnos").value;
+        oznaciVrstico(trenutnaVrstica);
+        var vrstica = naloziVrstico(izbraneVaje, trenutnaVaja-1, trenutnaVrstica);
+        
+        if (vnos == vrstica.substring(0,vnos.length)){
+        }
+        else{
+            vnos = vrstica.substring(0, vnos.length-1);
+            document.getElementById("vnos").value = vnos;
+            napakeVaja++;
+            vseNapake++;
+
+        }
+        udarciVaja++;
+        vsiUdarci++;
+        napakeProc = Math.round((napakeVaja / udarciVaja) * 10000) / 100;
+        napakeProc = napakeProc.toFixed(2);
+        vseNapakeProc = Math.round((vseNapake / vsiUdarci) * 10000) / 100;
+        vseNapakeProc = vseNapakeProc.toFixed(2);
+        udarciMin = (udarciVaja / ((ure * 3600) + (minute * 60) + sekunde)) * 60;
+        skupniUdarciMin = (vsiUdarci / ((vseUre * 3600) + (vseMinute * 60) + vseSekunde)) * 60;
+
+        if (preveriVrstico(vnos, vrstica)){
+            document.getElementById("vnos").value = vnos.substring(0, 0);
+            if (trenutnaVrstica == 2){
+                trenutnaVrstica = 0;
+                opravljeneVaje++;
+                if ((trenutnaVaja + 1) > preveriVaje(naloziVaje(izbraneVaje))){
+                    konecNabora();
+                    konecVaje = 1;
+
+                }
+                else{
+                    trenutnaVaja++;                    
+                    zacetekVaje = 0;
+                    naloziVajo(izbraneVaje, trenutnaVaja-1);
+                    udarciVaja = 0;
+                    napakeVaja = 0;
+                    napakeProc = 0;
+                }
+            }
+            else{
+                trenutnaVrstica++;
+            }
+            oznaciVrstico(trenutnaVrstica);
+        }
+    }
+
+    posodobi();
+}
---
+++ js/teksti.js
@@ -0,0 +1,252 @@
+var asdf = [
+    [
+        "fjfj fjfj fjfj fjfj fjfj fjfj fjfj fjfj",
+        "jffj jffj jffj jffj jffj jffj jffj jffj",
+        "fjfj jffj fjfj jffj fjfj jffj fjfj jffj"
+    ],
+    [
+        "FJF FJF FJF FJF FJF FJF FJF FJF FJF FJF",
+        "JFJ JFJ JFJ JFJ JFJ JFJ JFJ JFJ JFJ JFJ",
+        "FJF JFJ FJF JFJ FJF JFJ FJF JFJ FJF JFJ"
+    ],
+    [
+        "ačač ačač ačač ačač ačač ačač ačač ačač",
+        "čaač čaač čaač čaač čaač čaač čaač čaač",
+        "ačač čaač ačač čaač ačač čaač ačač čaač"
+    ],
+    [
+        "ČAČA ČAČA ČAČA ČAČA ČAČA ČAČA ČAČA ČAČA",
+        "AČČA AČČA AČČA AČČA AČČA AČČA AČČA AČČA",
+        "ČAČA AČČA ČAČA AČČA ČAČA AČČA ČAČA AČČA"
+    ],
+    [
+        "afjč afjč afjč afjč afjč afjč afjč afjč",
+        "jčfa jčfa jčfa jčfa jčfa jčfa jčfa jčfa",
+        "afjč jčfa afjč jčfa afjč jčfa afjč jčfa"
+    ],
+    [
+        "fajč fajč fajč fajč fajč fajč fajč fajč",
+        "čajf čajf čajf čajf čajf čajf čajf čajf",
+        "fajč fajč fajč fajč fajč fajč fajč fajč"
+    ],
+    [
+        "jafa jafa jafa jafa jafa jafa jafa jafa",
+        "čajč čajč čajč čajč čajč čajč čajč čajč",
+        "jafa čajč jafa čajč jafa čajč jafa čajč"
+    ],
+    [
+        "čaj čaj čaj čaj čaj čaj čaj čaj čaj čaj",
+        "aja aja aja aja aja aja aja aja aja aja",
+        "čaj aja čaj aja čaj aja čaj aja čaj aja"
+    ],
+    [
+        "dkdk dkdk dkdk dkdk dkdk dkdk dkdk dkdk",
+        "kddk kddk kddk kddk kddk kddk kddk kddk",
+        "dkdk kddk dkdk kddk dkdk kddk dkdk kddk"
+    ],
+    [
+        "fdjk fdjk fdjk fdjk fdjk fdjk fdjk fdjk",
+        "adčk adčk adčk adčk adčk adčk adčk adčk",
+        "fdjk adčk fdjk adčk fdjk adčk fdjk adčk"
+    ],
+    [
+        "adf jkč adf jkč adf jkč adf jkč adf jkč",
+        "fda čkj fda čkj fda čkj fda čkj fda čkj",
+        "daf kčj daf kčj daf kčj daf kčj daf kčj"
+    ],
+    [
+        "jada kada jada kada jada kada jada kada",
+        "jafa daka jafa daka jafa daka jafa daka",
+        "kaka čaka kaka čaka kaka čaka kaka čaka"
+    ],
+    [
+        "ada kaj ada kaj ada kaj ada kaj ada kaj",
+        "daj kad daj kad daj kad daj kad daj kad",
+        "jaka čaka jaka čaka jaka čaka jaka čaka"
+    ],
+    [
+        "ajd čaj ajd čaj ajd čaj ajd čaj ajd čaj",
+        "ajč jad ajč jad ajč jad ajč jad ajč jad",
+        "jajč kajf jajč kajf jajč kajf jajč kajf"
+    ],
+    [
+        "slsl slsl slsl slsl slsl slsl slsl slsl",
+        "lssl lssl lssl lssl lssl lssl lssl lssl",
+        "slsl lsls slsl lsls slsl lsls slsl lsls"
+    ],
+    [
+        "sfjl sfjl sfjl sfjl sfjl sfjl sfjl sfjl",
+        "asčl asčl asčl asčl asčl asčl asčl asčl",
+        "sdlk sdlk sdlk sdlk sdlk sdlk sdlk sdlk"
+    ],
+    [
+        "asdf jklč asdf jklč asdf jklč asdf jklč",
+        "člkj fdsa člkj fdsa člkj fdsa člkj fdsa",
+        "ačač slsl dkdk fjfj ačač slsl dkdk fjfj"
+    ],
+    [
+        "jaaj ajaj jaaj ajaj jaaj ajaj jaaj ajaj",
+        "fask kask fask kask fask kask fask kask",
+        "las sla las sla las sla las sla las sla"
+    ],
+    [
+        "das kas das kas das kas das kas das kas",
+        "sad jad sad jad sad jad sad jad sad jad",
+        "fasa jasa fasa jasa fasa jasa fasa jasa"
+    ],
+    [
+        "asad kasa asad kasa asad kasa asad kasa",
+        "kala dasa kala dasa kala dasa kala dasa",
+        "lada kajf lada kajf lada kajf lada kajf"
+    ],
+    [
+        "ladja ladja ladja ladja ladja ladja ladja",
+        "skala skala skala skala skala skala skala",
+        "čakal čakal čakal čakal čakal čakal čakal"
+    ],
+    [
+        "čaj las čaj las čaj las čaj las čaj las",
+        "kas saj kas saj kas saj kas saj kas saj",
+        "čaj las kas saj čaj las kas saj čaj las"
+    ],
+    [
+        "jaka kaka jaka kaka jaka kaka jaka kaka",
+        "slak čaka slak čaka slak čaka slak čaka",
+        "sjaj klas sjaj klas sjaj klas sjaj klas"
+    ],
+    [
+        "dala dala dala dala dala dala kača kača kača kača kača kača",
+        "kdaj kdaj kdaj kdaj kdaj kdaj ajda ajda ajda ajda ajda ajda",
+        "ajda kača kdaj dala ajda kača kdaj dala ajda kača kdaj dala"
+    ],
+    [
+        "a č d f j k l s s l k j f d č a a č d f a č d f j k l s s l",
+        "ač sl dk fj jf kd ls ča ač sl dk fj ač sl dk fj jf kd ls ča",
+        "dskl klds ačsl slač čakd kdač fjsl slfj dskl klds ačsl slač"
+    ],
+    [
+        "jaka čaka čaj jaka čaka čaj jaka čaka čaj jaka čaka čaj",
+        "ada daj čaja ada daj čaja ada daj čaja ada daj čaja ada",
+        "dala sladak sladak čaj dala sladak sladak čaj dala sladak"
+    ],
+    [
+        "ladja čakala čas ladja čakala čas ladja čakala čas ladja",
+        "kača skakala kača skakala kača skakala kača skakala kača",
+        "kdaj čaka ladja kdaj čaka ladja kdaj čaka ladja kdaj čaka"
+    ],
+    [
+        "skala skakljala skala skakljala skala skakljala skala",
+        "jajčka kakala jajčka kakala jajčka kakala jajčka kakala",
+        "fasada safalada fasada safalada fasada safalada fasada"
+    ],
+    [
+        "dlaka jasa klas skakač lada fala sladak dajal fasada safala",
+        "kača ladja jajčka čakala lajala slak skala fafa jaffa las",
+        "jkla kldd člkj adkl jfkd slls dadl čjks sfak kllk slds lkaf"
+    ],
+    [
+        "fFj fFj fFj fFj fFj fFj fFj fFj fFj fFj ",
+        "jJf jJf jJf jJf jJf jJf jJf jJf jJf jJf",
+        "fFjJ fFjJ fFjJ fFjJ fFjJ fFjJ fFjJ fFjJ "
+    ],
+    [
+        "fgfg fgfg fgfg fgfg fgfg fgfg fgfg fgfg ",
+        "jhjh jhjh jhjh jhjh jhjh jhjh jhjh jhjh ",
+        "fgfg jhjh fgfg jhjh fgfg jhjh fgfg jhjh "
+    ],
+    [
+        "agf agf agf agf agf agf agf agf agf agf",
+        "čhj čhj čhj čhj čhj čhj čhj čhj čhj čhj",
+        "agf čhj agf čhj agf čhj agf čhj agf čhj"
+    ],
+    [
+        "gaj gaj gaj gaj gaj gaj gaj gaj gaj gaj",
+        "lah lah lah lah lah lah lah lah lah lah",
+        "haga haga haga haga haga haga haga haga"
+    ],
+    [
+        "gčgč haha gčgč haha gčgč haha gčgč haha",
+        "glgl hshs glgl hshs glgl hshs glgl hshs",
+        "gčhč haha glgl hshs gčhč haha glgl hshs"
+    ],
+    [
+        "alga jaha alga jaha alga jaha alga jaha",
+        "gala hlad gala hlad gala hlad gala hlad",
+        "halja gaj halja gaj halja gaj halja gaj"
+    ],
+    [
+        "gala gala halja halja alga alga lah lah gaga gaga glas glas",
+        "lagala lagala lahka lahka gladka gladka haha haha hlad hlad",
+        "gas gas aha aha sladak sladak alah alah hala hala kgkg kgkg"
+    ],
+    [
+        "kg hl dag kg hl dag kg hl dag kg hl dag kg hl dag kg hl dag",
+        "lahka al ga lahka al ga lahka al ga lahka al ga lahka al ga",
+        "jahača glas jahača glas jahača glas jahača glas jahača glas"
+    ],
+    [
+        "gala hlad hala alga gala hlad hala alga gala hlad hala alga",
+        "gad ga čaka gad ga čaka gad ga čaka gad ga čaka gad ga čaka",
+        "halja da hlad halja da hlad halja da hlad halja da hlad halja"
+    ],
+    [
+        "kg hl gad haha gala klada sag jh halja ga čakala dajal glas",
+        "glas kg hl gad halja ga čakala dajal haha gala sklad sag ja",
+        "gad haha gala halja ga glas kg hl čakala dajal klada aga al"
+    ],
+    [
+        "gGj gGj gGj gGj gGj gGj gGj gGj gGj gGj",
+        "hHf hHf hHf hHf hHf hHf hHf hHf hHf hHf",
+        "gGhH gGhH gGhH gGhH gGhH gGhH gGhH gGhH"
+    ],
+    [
+        "frfr frfr frfr frfr frfr frfr frfr frfr",
+        "juju juju juju juju juju juju juju juju",
+        "frfr juju frfr juju frfr juju frfr juju"
+    ],
+    [
+        "frju frju frju frju frju frju frju frju ",
+        "rfuj rfuj rfuj rfuj rfuj rfuj rfuj rfuj ",
+        "frju rfuj frju rfuj frju rfuj frju rfuj "
+    ],
+    [
+        "fujr fujr fujr fujr fujr fujr fujr fujr",
+        "jrfu jrfu jrfu jrfu jrfu jrfu jrfu jrfu",
+        "fujr jrfu fujr jrfu fujr jrfu fujr jrfu"
+    ],
+    [
+        "frju fgjh frju fgjh frju fgjh frju fgjh",
+        "frfg jujh frfg jujh frfg jujh frfg jujh",
+        "frju fgjh frfg jujh frju fgjh frfg jujh"
+    ],
+    [
+        "asdr člku asdr člku asdr člku asdr člku",
+        "frag juhč frag juhč frag juhč frag juhč",
+        "kura frak kura frak kura frak kura frak"
+    ],
+    [
+        "juha kruh juha kruh juha kruh juha kruh juha kruh juha kruh",
+        "grah urar grah urar grah urar grah urar grah urar grah urar",
+        "udar ragu udar ragu udar ragu udar ragu udar ragu udar ragu"
+    ],
+    [
+        "srka srka sluh sluh luka luka kuku kuku grda grda gara gara",
+        "črka črka krka krka raga raga hula hula urad urad frka frka",
+        "rska rska hura hura dara dara rada rada kura kura urka urka"
+    ],
+    [
+        "daruj daruj kafra kafra ključ ključ rudar rudar garač garač",
+        "gusar gusar kuhar kuhar lučka lučka račka račka ključ ključ",
+        "dragulj dragulj kragulj kragulj čuk čuk črkrj črkrj ura ura"
+    ],
+    [
+        "ljudska ruda urar ragu luka lučka kuhar grah čula juha kruh",
+        "safalada ključ rudar garač sraga dragulj sadjar kafra druga",
+        "luksus lučka gruča daruj kar gruda črkuj gusar čudak slučaj"
+    ],
+    [
+        "ključ jadra kufra julska laska urar luska druga sraka kuhar",
+        "črkuj fasada ugajaj gladka kljuka jug srka gad sadjar graja",
+        "ga rad kruh čaka kurjač dada ga huda srka suh ura rada čaka"
+    ]
+];
